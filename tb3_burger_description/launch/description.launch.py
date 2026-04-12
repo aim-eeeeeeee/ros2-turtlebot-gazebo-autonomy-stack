dce9c7561402097bbc17a8b26dcaae9e73fb673f
aim-eeeeeeee <azheng625@gmail.com>
2026-04-12 15:28:07 -0400

    fix

--- tb3_burger_description/launch/description.launch.py
+++ tb3_burger_description/launch/description.launch.py
@@ -2,6 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
 
@@ -10,6 +11,7 @@ def generate_launch_description():
     urdf_file = os.path.join(pkg_share, 'urdf', 'tb3.urdf.xacro')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    use_joint_state_gui = LaunchConfiguration('use_joint_state_gui', default='false')
 
     robot_description = Command(['xacro ', urdf_file])
 
@@ -18,6 +20,11 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use simulation clock if true'
+        ),
+        DeclareLaunchArgument(
+            'use_joint_state_gui',
+            default_value='false',
+            description='Launch joint_state_publisher_gui for manual joint testing.'
         ),
 
         Node(
@@ -36,5 +43,6 @@ def generate_launch_description():
             executable='joint_state_publisher_gui',
             name='joint_state_publisher_gui',
             output='screen',
+            condition=IfCondition(use_joint_state_gui),
         ),
     ])
